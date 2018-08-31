c10290174b9d83972454e985a9a511fc329882f4
vindell <hnxyhcwdl1003@163.com>
2018-08-31 21:53:44 +0800

    修改

--- src/main/java/org/apache/shiro/spring/boot/ShiroBizWebAutoConfiguration.java
+++ src/main/java/org/apache/shiro/spring/boot/ShiroBizWebAutoConfiguration.java
@@ -1,44 +1,108 @@
 package org.apache.shiro.spring.boot;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.collections.MapUtils;
+import org.apache.shiro.biz.realm.PrincipalRealmListener;
+import org.apache.shiro.biz.web.filter.authc.listener.LoginListener;
+import org.apache.shiro.biz.web.filter.authc.listener.LogoutListener;
+import org.apache.shiro.mgt.SessionsSecurityManager;
+import org.apache.shiro.realm.Realm;
 import org.apache.shiro.session.mgt.SessionManager;
-import org.apache.shiro.spring.boot.biz.ShiroRememberMeManagerCookieProperties;
-import org.apache.shiro.spring.boot.biz.ShiroSessionManagerCookieProperties;
-import org.apache.shiro.spring.boot.biz.ShiroSessionManagerProperties;
 import org.apache.shiro.spring.web.config.AbstractShiroWebConfiguration;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
-import org.apache.shiro.web.servlet.Cookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
-import org.apache.shiro.web.session.mgt.ServletContainerSessionManager;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
 
 @Configuration
 @AutoConfigureBefore(name = { "org.apache.shiro.spring.config.web.autoconfigure.ShiroWebAutoConfiguration" // shiro-spring-boot-web-starter
 })
 @ConditionalOnWebApplication
 @ConditionalOnProperty(prefix = ShiroBizProperties.PREFIX, value = "enabled", havingValue = "true")
-@EnableConfigurationProperties({ ShiroBizProperties.class, ShiroRememberMeManagerCookieProperties.class,
-		ShiroSessionManagerProperties.class, ShiroSessionManagerCookieProperties.class })
-public class ShiroBizWebAutoConfiguration extends AbstractShiroWebConfiguration {
+@EnableConfigurationProperties({ ShiroBizProperties.class})
+public class ShiroBizWebAutoConfiguration extends AbstractShiroWebConfiguration implements ApplicationContextAware {
+
+	private ApplicationContext applicationContext;
 
 	@Autowired
 	private ShiroBizProperties bizProperties;
-	@Autowired
-	private ShiroRememberMeManagerCookieProperties rememberMeProperties;
-	@Autowired
-	private ShiroSessionManagerProperties sessionManagerProperties;
-	@Autowired
-	private ShiroSessionManagerCookieProperties sessionManagerCookieProperties;
+
+	/**
+	 * 登录监听：实现该接口可监听账号登录失败和成功的状态，从而做业务系统自己的事情，比如记录日志
+	 */
+	@Bean("loginListeners")
+	@ConditionalOnMissingBean(name = "loginListeners")
+	public List<LoginListener> loginListeners() {
+
+		List<LoginListener> loginListeners = new ArrayList<LoginListener>();
+
+		Map<String, LoginListener> beansOfType = getApplicationContext().getBeansOfType(LoginListener.class);
+		if (!ObjectUtils.isEmpty(beansOfType)) {
+			Iterator<Entry<String, LoginListener>> ite = beansOfType.entrySet().iterator();
+			while (ite.hasNext()) {
+				loginListeners.add(ite.next().getValue());
+			}
+		}
+
+		return loginListeners;
+	}
+
+	/**
+	 * Realm 执行监听：实现该接口可监听认证失败和成功的状态，从而做业务系统自己的事情，比如记录日志
+	 */
+	@Bean("realmListeners")
+	@ConditionalOnMissingBean(name = "realmListeners")
+	public List<PrincipalRealmListener> realmListeners() {
+
+		List<PrincipalRealmListener> realmListeners = new ArrayList<PrincipalRealmListener>();
+
+		Map<String, PrincipalRealmListener> beansOfType = getApplicationContext()
+				.getBeansOfType(PrincipalRealmListener.class);
+		if (!ObjectUtils.isEmpty(beansOfType)) {
+			Iterator<Entry<String, PrincipalRealmListener>> ite = beansOfType.entrySet().iterator();
+			while (ite.hasNext()) {
+				realmListeners.add(ite.next().getValue());
+			}
+		}
+
+		return realmListeners;
+	}
+
+	/**
+	 * 注销监听：实现该接口可监听账号注销失败和成功的状态，从而做业务系统自己的事情，比如记录日志
+	 */
+	@Bean("logoutListeners")
+	@ConditionalOnMissingBean(name = "logoutListeners")
+	public List<LogoutListener> logoutListeners() {
+
+		List<LogoutListener> logoutListeners = new ArrayList<LogoutListener>();
+
+		Map<String, LogoutListener> beansOfType = getApplicationContext().getBeansOfType(LogoutListener.class);
+		if (!ObjectUtils.isEmpty(beansOfType)) {
+			Iterator<Entry<String, LogoutListener>> ite = beansOfType.entrySet().iterator();
+			while (ite.hasNext()) {
+				logoutListeners.add(ite.next().getValue());
+			}
+		}
+
+		return logoutListeners;
+	}
 
 	/**
 	 * 责任链定义 ：定义Shiro的逻辑处理责任链
@@ -58,40 +122,59 @@ public class ShiroBizWebAutoConfiguration extends AbstractShiroWebConfiguration 
 		chainDefinition.addPathDefinition("/**", "authc");
 		return chainDefinition;
 	}
-
-    @Bean
-	@Override
-    protected SessionManager sessionManager() {
-        if (sessionManagerProperties.isUserNative()) {
-        	
-        	 DefaultWebSessionManager webSessionManager = new DefaultWebSessionManager();
-             webSessionManager.setSessionIdCookieEnabled(sessionManagerProperties.isSessionIdCookieEnabled());
-             webSessionManager.setSessionIdUrlRewritingEnabled(sessionManagerProperties.isSessionIdUrlRewritingEnabled());
-             webSessionManager.setSessionIdCookie(sessionCookieTemplate());
-             
-             webSessionManager.setSessionFactory(sessionFactory());
-             webSessionManager.setSessionDAO(sessionDAO());
-             webSessionManager.setDeleteInvalidSessions(sessionManagerProperties.isDeleteInvalidSessions());
-
-             webSessionManager.setCacheManager(cacheManager);
-             
-             return webSessionManager;
-             
-        }
-        return new ServletContainerSessionManager();
-    }
 	
+	@Bean
 	@Override
-	protected Cookie sessionCookieTemplate() {
-		return buildCookie(sessionManagerCookieProperties.getName(), sessionManagerCookieProperties.getMaxAge(),
-				sessionManagerCookieProperties.getPath(), sessionManagerCookieProperties.getDomain(),
-				sessionManagerCookieProperties.isSecure());
+	protected SessionManager sessionManager() {
+		SessionManager sessionManager = super.sessionManager();
+		if (sessionManager instanceof DefaultWebSessionManager) {
+			DefaultWebSessionManager webSessionManager = (DefaultWebSessionManager) sessionManager;
+			webSessionManager.setCacheManager(cacheManager);
+			return webSessionManager;
+
+		}
+		return sessionManager;
 	}
 
+	@Bean
+	@Override
+	protected SessionsSecurityManager securityManager(List<Realm> realms) {
+		return super.securityManager(realms);
+	}
+
+	/*@Bean
+    @ConditionalOnMissingBean(name = "sessionCookieTemplate")
+	@Override
+	protected Cookie sessionCookieTemplate() {
+		if (sessionManagerCookieProperties != null && StringUtils.hasText(sessionManagerCookieProperties.getName())
+				&& sessionManagerCookieProperties.getMaxAge() > 0) {
+			return buildCookie(sessionManagerCookieProperties.getName(), sessionManagerCookieProperties.getMaxAge(),
+					sessionManagerCookieProperties.getPath(), sessionManagerCookieProperties.getDomain(),
+					sessionManagerCookieProperties.isSecure());
+		}
+		return super.sessionCookieTemplate();
+	}
+
+	@Bean
+    @ConditionalOnMissingBean(name = "rememberMeCookieTemplate")
 	@Override
 	protected Cookie rememberMeCookieTemplate() {
-		return buildCookie(rememberMeProperties.getName(), rememberMeProperties.getMaxAge(),
-				rememberMeProperties.getPath(), rememberMeProperties.getDomain(), rememberMeProperties.isSecure());
+		if (rememberMeProperties != null && StringUtils.hasText(rememberMeProperties.getName())
+				&& rememberMeProperties.getMaxAge() > 0) {
+			return buildCookie(rememberMeProperties.getName(), rememberMeProperties.getMaxAge(),
+					rememberMeProperties.getPath(), rememberMeProperties.getDomain(), rememberMeProperties.isSecure());
+		}
+		return super.rememberMeCookieTemplate();
+
+	}
+*/
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
 	}
 
 }
--- src/main/java/org/apache/shiro/spring/boot/ShiroBizWebFilterConfiguration.java
+++ src/main/java/org/apache/shiro/spring/boot/ShiroBizWebFilterConfiguration.java
@@ -1,22 +1,15 @@
 package org.apache.shiro.spring.boot;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
-import org.apache.shiro.biz.realm.PrincipalRealmListener;
 import org.apache.shiro.biz.web.filter.HttpServletSessionControlFilter;
 import org.apache.shiro.biz.web.filter.HttpServletSessionExpiredFilter;
-import org.apache.shiro.biz.web.filter.authc.listener.LoginListener;
 import org.apache.shiro.biz.web.filter.authc.listener.LogoutListener;
 import org.apache.shiro.spring.boot.biz.ShiroBizFilterFactoryBean;
 import org.apache.shiro.spring.boot.biz.authc.BizLogoutFilter;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.spring.web.config.AbstractShiroWebFilterConfiguration;
 import org.apache.shiro.web.servlet.AbstractShiroFilter;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -24,12 +17,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.util.ObjectUtils;
 
 /**
  * 默认拦截器
@@ -221,77 +211,14 @@ import org.springframework.util.ObjectUtils;
 @Configuration
 @AutoConfigureBefore( name = {
 	"org.apache.shiro.spring.config.web.autoconfigure.ShiroWebFilterConfiguration", // shiro-spring-boot-web-starter
-	"org.apache.shiro.spring.boot.ShiroBizWebAutoConfiguration"
 })
 @ConditionalOnWebApplication
 @ConditionalOnProperty(prefix = ShiroBizProperties.PREFIX, value = "enabled", havingValue = "true")
 @EnableConfigurationProperties({ ShiroBizProperties.class })
-public class ShiroBizWebFilterConfiguration extends AbstractShiroWebFilterConfiguration implements ApplicationContextAware {
+public class ShiroBizWebFilterConfiguration extends AbstractShiroWebFilterConfiguration {
 
-	private ApplicationContext applicationContext;
-	
 	@Autowired
 	private ShiroBizProperties bizProperties;
-	
-	/**
-	 * 登录监听：实现该接口可监听账号登录失败和成功的状态，从而做业务系统自己的事情，比如记录日志
-	 */
-	@Bean("loginListeners")
-	@ConditionalOnMissingBean(name = "loginListeners")
-	public List<LoginListener> loginListeners() {
-
-		List<LoginListener> loginListeners = new ArrayList<LoginListener>();
-		
-		Map<String, LoginListener> beansOfType = getApplicationContext().getBeansOfType(LoginListener.class);
-		if (!ObjectUtils.isEmpty(beansOfType)) {
-			Iterator<Entry<String, LoginListener>> ite = beansOfType.entrySet().iterator();
-			while (ite.hasNext()) {
-				loginListeners.add(ite.next().getValue());
-			}
-		}
-		
-		return loginListeners;
-	}
-	
-	/**
-	 * Realm 执行监听：实现该接口可监听认证失败和成功的状态，从而做业务系统自己的事情，比如记录日志
-	 */
-	@Bean("realmListeners")
-	@ConditionalOnMissingBean(name = "realmListeners")
-	public List<PrincipalRealmListener> realmListeners() {
-
-		List<PrincipalRealmListener> realmListeners = new ArrayList<PrincipalRealmListener>();
-		
-		Map<String, PrincipalRealmListener> beansOfType = getApplicationContext().getBeansOfType(PrincipalRealmListener.class);
-		if (!ObjectUtils.isEmpty(beansOfType)) {
-			Iterator<Entry<String, PrincipalRealmListener>> ite = beansOfType.entrySet().iterator();
-			while (ite.hasNext()) {
-				realmListeners.add(ite.next().getValue());
-			}
-		}
-		
-		return realmListeners;
-	}
-	
-	/**
-	 * 注销监听：实现该接口可监听账号注销失败和成功的状态，从而做业务系统自己的事情，比如记录日志
-	 */
-	@Bean("logoutListeners")
-	@ConditionalOnMissingBean(name = "logoutListeners")
-	public List<LogoutListener> logoutListeners() {
-
-		List<LogoutListener> logoutListeners = new ArrayList<LogoutListener>();
-		
-		Map<String, LogoutListener> beansOfType = getApplicationContext().getBeansOfType(LogoutListener.class);
-		if (!ObjectUtils.isEmpty(beansOfType)) {
-			Iterator<Entry<String, LogoutListener>> ite = beansOfType.entrySet().iterator();
-			while (ite.hasNext()) {
-				logoutListeners.add(ite.next().getValue());
-			}
-		}
-		
-		return logoutListeners;
-	}
 	
 	/**
 	 * 系统登录注销过滤器；默认：org.apache.shiro.spring.boot.cas.filter.CasLogoutFilter
@@ -381,14 +308,5 @@ public class ShiroBizWebFilterConfiguration extends AbstractShiroWebFilterConfig
 
         return filterRegistrationBean;
     }
-	
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
-
-	public ApplicationContext getApplicationContext() {
-		return applicationContext;
-	}
 
 }
